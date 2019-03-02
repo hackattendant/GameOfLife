3e8bfa69203089135479462fa0a86f757cbe286b
Ethan Peacock <peacockethan@gmail.com>
2019-03-02 16:26:28 -0500

    initial commit

---
+++ life.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+# (c) Ethan Peacock
+# ----------------------------------------------------------------------------------------------
+
+# imports
+import numpy as np
+
+# configurations
+glider = np.array([[1, 0, 0],
+                   [0, 1, 1],
+                   [1, 1, 0]])
+
+
+
+if __name__ == "__main__":
